8b0baa3aaf43e82ab363bc05aefa8fad53d87203
ElWapo <cdmx2125@amerike.edu.mx>
2025-04-01 18:56:22 -0600

    InputActions

---
+++ Assets/Scripts/S_Stats.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_Stats 
+{
+    public int health;
+    public int shieldhealth;
+    public int ammo1;
+    public int ammo2;
+    public int speed;
+    public int damage;
+}
